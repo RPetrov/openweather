356813e4735326bca30676f232237e81f2a28254
Your Name <you@example.com>
2024-02-27 19:23:21 +0300

    Решение спринта 9

--- app/src/main/java/rpetrov/openweather/MainActivity.kt
+++ app/src/main/java/rpetrov/openweather/MainActivity.kt
@@ -21,9 +21,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // TODO()
-
-        TODO()
 
         // new changes
     }
