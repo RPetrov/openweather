6be044685f735a93ec9eac9b8b5cfc0e669590d6
Your Name <you@example.com>
2024-05-15 15:37:50 +0300

    request location

--- app/src/main/java/rpetrov/openweather/MainActivity.kt
+++ app/src/main/java/rpetrov/openweather/MainActivity.kt
@@ -1,27 +1,57 @@
 package rpetrov.openweather
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 /**
- * - Создание веток в гит ( ветки для спринта, разные пути, потом все мержим в dev, потом в main )
- * - Отдельная ветка чтобы “показать” наставнику проблему
- * - Создание PR
- * - Краши: стектрейсы, дебаг.
- * - Лейаут-инспектор - пример того, как работает, как решает проблему.
- * - Анализатор кода
- * - Адаптер и клики, и сохранение данных в отдельной сущности
- *
  * - Запросить местоположение
- * - Получить погоду c помощью либы
+ * - Получить текущую погоду и отобразить её
+ * - Получить получить прогноз
  * - построить список
- * - два холдера?
  */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var locationProvider: FusedLocationProviderClient
+
+    val registerForActivityResult = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { hasPermission ->
+
+        if (!hasPermission) {
+            Toast.makeText(this@MainActivity, "No permission", Toast.LENGTH_SHORT).show()
+        } else {
+            getAndShowCurrentWeather()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // new changes
+        locationProvider = LocationServices.getFusedLocationProviderClient(this)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED) {
+            registerForActivityResult.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+        } else {
+            getAndShowCurrentWeather()
+        }
+    }
+
+    fun getAndShowCurrentWeather() {
+        locationProvider.lastLocation.addOnSuccessListener { location ->
+            Toast.makeText(this@MainActivity, "location: ${location}", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(this@MainActivity, "error ${it.message}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
