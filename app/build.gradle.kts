0c6d6cec03526a18a9d48dc09d7fd008d8ef9d34
ИÐIgor Starkov <Kazesteam@yandex.ru>
2024-06-26 21:04:25 +0300

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.squareup.retrofit2:converter-gson:+")
 
     implementation("com.squareup.retrofit2:retrofit:+")
     implementation("com.google.code.gson:gson:2.11.0")
--- app/src/main/java/rpetrov/openweather/MainActivity.kt
+++ app/src/main/java/rpetrov/openweather/MainActivity.kt
@@ -1,16 +1,23 @@
 package rpetrov.openweather
 
 import android.Manifest
-import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import rpetrov.openweather.models.curentwheather.CurrentWheather
 
 /**
  * - Запросить местоположение
@@ -21,17 +28,44 @@ import com.google.android.gms.location.LocationServices
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val weatherServise = retrofit.create(WeatherApi::class.java)
+    var tempTextView: TextView? = null
+    var humidityTextView: TextView? = null
+    var discriptionTextView: TextView? = null
+
 
     val registerForResult = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
         if (it) {
             fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
             if (ActivityCompat.checkSelfPermission(
                     this, Manifest.permission.ACCESS_COARSE_LOCATION
-                ) == PackageManager.PERMISSION_GRANTED) {
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
                 fusedLocationClient.lastLocation.addOnSuccessListener {
                     val location = it
                     val lat = location.latitude
                     val lon = location.longitude
+                    weatherServise.getCurrentWeather(lat, lon)
+                        .enqueue(object : Callback<CurrentWheather> {
+                            override fun onResponse(
+                                p0: Call<CurrentWheather>,
+                                p1: Response<CurrentWheather>
+                            ) {
+                                if (p1.isSuccessful) {
+                                    val temperature = p1.body()?.main?.temp
+                                    tempTextView?.text = temperature.toString()
+                                }
+                            }
+
+                            override fun onFailure(p0: Call<CurrentWheather>, p1: Throwable) {
+                                Log.d(TAG, "$p1", p1)
+                            }
+                        })
                 }
             }
 
@@ -45,9 +79,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         registerForResult.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+        initViews()
     }
 
     override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
         super.onSaveInstanceState(outState, outPersistentState)
+    }
+
+    private fun initViews() {
+        tempTextView = findViewById(R.id.temp)
+        humidityTextView = findViewById(R.id.hum)
+        discriptionTextView = findViewById(R.id.desc)
+    }
+
+    companion object {
+        const val BASE_URL = "https://api.openweathermap.org/"
+        const val TAG = "MainActivity"
     }
 }
