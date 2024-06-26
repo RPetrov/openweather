112105180ae3deb276febc419a85a473c39ea104
Your Name <you@example.com>
2024-06-26 18:42:46 +0300

    init

--- app/src/main/java/rpetrov/openweather/MainActivity.kt
+++ app/src/main/java/rpetrov/openweather/MainActivity.kt
@@ -3,6 +3,12 @@ package rpetrov.openweather
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * - Запросить местоположение
+ * - Получить текущую погоду и отобразить её
+ * - Получить получить прогноз
+ * - построить список
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
